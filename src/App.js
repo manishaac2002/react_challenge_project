e0d5b6f8d2b60b8cb90f59e9362e7ab075d898bc
manishaac2002 <manishaac2002@gmail.com>
2024-10-08 15:07:25 +0530

    chore--

--- src/App.js
+++ src/App.js
@@ -1,67 +1,44 @@
-import {useState} from 'react'
-import "./index.css";
+import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <FlashCards />
+    <div>
+      <TextExpander>
+        Space travel is the ultimate adventure! Imagine soaring past the stars
+        and exploring new worlds. It's the stuff of dreams and science fiction,
+        but believe it or not, space travel is a real thing. Humans and robots
+        are constantly venturing out into the cosmos to uncover its secrets and
+        push the boundaries of what's possible.
+      </TextExpander>
+
+      <TextExpander
+        collapsedNumWords={20}
+        expandButtonText="Show text"
+        collapseButtonText="Collapse text"
+        buttonColor="#ff6622"
+      >
+        Space travel requires some seriously amazing technology and
+        collaboration between countries, private companies, and international
+        space organizations. And while it's not always easy (or cheap), the
+        results are out of this world. Think about the first time humans stepped
+        foot on the moon or when rovers were sent to roam around on Mars.
+      </TextExpander>
+
+      <TextExpander expanded={true} className="box">
+        Space missions have given us incredible insights into our universe and
+        have inspired future generations to keep reaching for the stars. Space
+        travel is a pretty cool thing to think about. Who knows what we'll
+        discover next!
+      </TextExpander>
     </div>
   );
 }
 
-
-const questions = [
-  {
-    id: 3457,
-    question: "What language is React based on?",
-    answer: "JavaScript"
-  },
-  {
-    id: 7336,
-    question: "What are the building blocks of React apps?",
-    answer: "Components"
-  },
-  {
-    id: 8832,
-    question: "What's the name of the syntax we use to describe a UI in React?",
-    answer: "JSX"
-  },
-  {
-    id: 1297,
-    question: "How to pass data from parent to child components?",
-    answer: "Props"
-  },
-  {
-    id: 9103,
-    question: "How to give components memory?",
-    answer: "useState hook"
-  },
-  {
-    id: 2002,
-    question:
-      "What do we call an input element that is completely synchronised with state?",
-    answer: "Controlled element"
-  }
-];
-
-function FlashCards() {
-  const [selectedId, setSelectedId] = useState(null)
-
-  function handleClick(id) {
-    setSelectedId(id !== selectedId ? id : null)
-  }
-
-  return (
-    <div className="flashcards" >
-
-      {questions.map((question) =>
-        (<div key={question.id} 
-          onClick={() => handleClick(question.id)} 
-          className={question.id === selectedId ? "selected" : ""}  >
-
-          <p>{question.id === selectedId ? question.answer : question.question}</p>
-
-          </div>)
-      )}
-    </div>)
+function TextExpander({
+  collapsedNumWords,
+  expandButtonText,
+  collapseButtonText,
+  buttonColor
+}) {
+  return <div>TODO</div>;
 }
